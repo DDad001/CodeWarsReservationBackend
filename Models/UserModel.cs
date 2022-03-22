3dc6f2914981bec771b9737a56eb843ee6e23b4e
Jovann Contreras <jcontreras@codestack.co>
2022-03-22 11:33:49 -0700

    Added new models

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -8,7 +8,7 @@ namespace CodeWarsReservationBackend.Models
     public class UserModel
     {
         public int Id { get; set; }
-        public string? Username { get; set; }
+        public string? CohortName { get; set; }
         public string? CodewarsName { get; set; }
         public string? Salt { get; set; }
         public string? Hash { get; set; }
