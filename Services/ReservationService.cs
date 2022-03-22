4cd8feb1121dc1105d44570b9ec4b235f5c1c84b
Danial <Danial.Dad248@gmail.com>
2022-03-22 15:49:48 -0700

    Pushing reservation service

--- Services/ReservationService.cs
+++ Services/ReservationService.cs
@@ -2,11 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodeWarsReservationBackend.Models;
+using CodeWarsReservationBackend.Services.Context;
 
 namespace CodeWarsReservationBackend.Services
 {
     public class ReservationService
     {
         
+        private readonly DataContext _context;
+
+       public ReservationService(DataContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<ReservationModel> GetAllReservedKatas()
+        {
+            return _context.ReservationInfo;
+        }
     }
 }
