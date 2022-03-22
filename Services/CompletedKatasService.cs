3339de8555fec5892514ae250268a9e50dccf644
Danial <Danial.Dad248@gmail.com>
2022-03-22 15:28:36 -0700

    Finished CompletedKatas

--- Services/CompletedKatasService.cs
+++ Services/CompletedKatasService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodeWarsReservationBackend.Models;
 using CodeWarsReservationBackend.Services.Context;
 
 namespace CodeWarsReservationBackend.Services
@@ -13,6 +14,14 @@ namespace CodeWarsReservationBackend.Services
         {
             _context = context;
         }
+
+        public IEnumerable<CompletedKatasModel> GetAllCompletedKatas()
+        {
+            return _context.CompletedKatasInfo;
+        }
+
+        
+
 
     }
 }
