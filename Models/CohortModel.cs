870c8eb23f3ee54a85fb0b9ca60909ea966e560c
Jovann Contreras <jcontreras@codestack.co>
2022-03-22 10:58:08 -0700

    Cohort Model

---
+++ Models/CohortModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeWarsReservationBackend.Models
+{
+    public class CohortModel
+    {
+        public int CohortId { get; set; }
+        public string? CohortName { get; set; }
+        public string? CohortLevelOfDifficulty { get; set; }
+    }
+}
